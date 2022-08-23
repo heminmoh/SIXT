c88a22a910c9c9be42303ffdb740ae2eb9bf0e00
Hemin Mohamadian <hh1989m@gmail.com>
2022-08-24 04:25:18 +0430

    2022-08-24 0430 Test Done!

--- app/src/androidTest/java/com/coding/sixt/remote/CheckResponseCodeTest.kt
+++ app/src/androidTest/java/com/coding/sixt/remote/CheckResponseCodeTest.kt
@@ -1,4 +1,4 @@
-package com.coding.sixt.fragments
+package com.coding.sixt.remote
 
 import android.content.Context
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -14,23 +14,14 @@ import retrofit2.await
 import retrofit2.awaitResponse
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-internal class CarsFragmentTest {
-
-
-
+internal class CheckResponseCodeTest {
     private lateinit var instrumentationContext: Context
-    private lateinit var mockObject : CarPreview
-
     @Test
     fun testretrofit() = runBlocking{
-        mockObject = CarPreview("WMWSW31030T222518","mini","MINI","Regine","BMW","MINI",
-            "midnight_black","MINI","P", 0.55,"M","",0.0,0.0,
-            "CLEAN","https://cdn.sixt.io/codingtask/images/mini.png")
         instrumentationContext = InstrumentationRegistry.getInstrumentation().targetContext
         val instance = ServiceBuilder(instrumentationContext).buildService(APIInterface::class.java)
         var response = instance.getCars().awaitResponse()
-        print(response.body()?.get(0)?.id)
 
-        Assert.assertEquals(response.body()?.get(0)?.id,mockObject.id)
+        Assert.assertEquals(200,response.code())
     }
 }
